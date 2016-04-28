0bc0a923e3da002917ec1ad0e3069bdd93278937
Florian Warg <florian.warg@hotmail.de>
2016-04-28 11:40:37 +0200

    Add expert solution for 06 - feedback

--- 06_feedback.c
+++ 06_feedback.c
@@ -8,12 +8,13 @@ int main() {
     char c;
     printf("Character: ");
     scanf("%c", &c);
-    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-        printf("%c is a letter.\n", c);
+    if (c >= 'A' && c <= 'Z')
+        printf("%c is an upper-case letter.\n", c);
+    else if (c >= 'a' && c <= 'z')
+        printf("%c is lower-case. Upper-case is %c", c, c + 'A' - 'a');
     else if (c >= '0' && c <= '9')
         printf("%c is a number.\n", c);
     else
         printf("%c is a special character.\n", c);
     return 0;
 }
-
