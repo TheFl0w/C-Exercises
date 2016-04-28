a81dec8f03bace488f1061e4c74ca658700f0f7a
Florian Warg <florian.warg@hotmail.de>
2016-04-28 12:28:24 +0200

    Add solution for lorem ipsum

---
+++ 16_lorem_ipsum.c
@@ -0,0 +1,16 @@
+/*
+ * 16 - Lorem Ipsum
+ */
+
+#include <stdio.h>
+
+int main() {
+    char str[] = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,\n"
+        "sed diam nonumy eirmod tempor invidunt ut labore et\n"
+        "dolore magna aliquyam erat, sed diam voluptua. At vero\n"
+        "eos et accusam et justo duo dolores et ea rebum.\n";
+    for (unsigned i = 0; i < sizeof(str); i++)
+        printf("%c", str[i]);
+    printf("\n");
+    return 0;
+}
