2a3e408ca92166eccaef1ba177867b98a3a7ce6b
Florian Warg <florian.warg@hotmail.de>
2016-05-26 12:35:02 +0200

    Add solution for exercise 20

---
+++ 20_complex_numbers.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+
+struct complex {
+    double real;
+    double imaginary;
+};
+
+struct complex scanComplex();
+void printComplex(struct complex c);
+struct complex addComplex(struct complex c, struct complex d);
+struct complex multComplex(struct complex c, struct complex d);
+
+struct complex scanComplex() {
+    struct complex result;
+    scanf("%lf + %lfi", &result.real, &result.imaginary);
+    return result;
+}
+
+void printComplex(struct complex c) {
+    printf("%lf + %lfi", c.real, c.imaginary);
+}
+
+struct complex addComplex(struct complex c, struct complex d) {
+    c.real += d.real;
+    c.imaginary += d.imaginary;
+    return c;
+}
+
+struct complex multComplex(struct complex c, struct complex d) {
+    struct complex result;
+    result.real = c.real * d.real - c.imaginary * d.imaginary;
+    result.imaginary = c.real * d.imaginary + c.imaginary * d.real;
+    return result;
+}
+
+int main() {
+    struct complex x, y;
+    printf("Input of x --> (a + bi): ");
+    x = scanComplex();
+    printf("Input of y --> (a + bi): ");
+    y = scanComplex();
+    puts("--------------------");
+    printf("x: ");
+    printComplex(x);
+    printf("\ny: ");
+    printComplex(y);
+    puts("\n--------------------");
+    printf("x + y: ");
+    printComplex(addComplex(x, y));
+    printf("\nx * y: ");
+    printComplex(multComplex(x, y));
+    puts("");
+    return 0;
+}
